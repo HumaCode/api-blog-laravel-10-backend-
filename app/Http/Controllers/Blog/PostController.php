46f5d820d1bfbdbb043aba1d3a0c7c97d1d7d328
HumaCode <HumaCode>
2023-09-07 20:54:23 +0700

    membuat method index - PostController

--- app/Http/Controllers/Blog/PostController.php
+++ app/Http/Controllers/Blog/PostController.php
@@ -3,9 +3,15 @@
 namespace App\Http\Controllers\Blog;
 
 use App\Http\Controllers\Controller;
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    public function index()
+    {
+        return response([
+            'posts' => Post::orderBy('created_at', 'desc')->with('user:id,name,image')->withCount('comments', 'like')->get(),
+        ], 200);
+    }
 }
