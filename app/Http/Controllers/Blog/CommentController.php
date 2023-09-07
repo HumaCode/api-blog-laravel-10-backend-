1cffdcb0fcb93a9e0d9494d8750d284249e53dfd
HumaCode <HumaCode>
2023-09-07 21:13:00 +0700

    membuat method store - CommentController

--- app/Http/Controllers/Blog/CommentController.php
+++ app/Http/Controllers/Blog/CommentController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Blog;
 
 use App\Http\Controllers\Controller;
+use App\Models\Comment;
 use App\Models\Post;
 use Illuminate\Http\Request;
 
@@ -21,6 +22,32 @@ class CommentController extends Controller
 
         return response([
             'post' => $post->comments()->with('user:id,name,email')->get()
+        ], 200);
+    }
+
+    public function store(Request $request, $id)
+    {
+        $post = Post::find($id);
+
+        // jika tidak ada post
+        if (!$post) {
+            return response([
+                'message' => 'Post not found..',
+            ], 404);
+        }
+
+        $attr = $request->validate([
+            'comment' => 'required|string',
         ]);
+
+        Comment::create([
+            'comment' => $attr['comment'],
+            'post_id' => $id,
+            'user_id' => auth()->user()->id
+        ]);
+
+        return response([
+            'message' => 'Comment created..',
+        ], 200);
     }
 }
