055376ba510f53800a513a098eb9d90a99e49f05
HumaCode <HumaCode>
2023-09-07 21:44:12 +0700

    membuat fungsi save image - base64

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,8 +5,21 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use PharIo\Manifest\Url;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    public function saveImage($image, $path = 'public')
+    {
+        if (!$image) {
+            return null;
+        }
+
+        $filename = time() . '.png';
+        \Storage::disk($path)->put($filename, base64_decode($image));
+
+        return Url::to('/') . '/storage/' . $path . '/' . $filename;
+    }
 }
