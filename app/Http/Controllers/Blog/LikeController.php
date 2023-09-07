dda8cb9b41bcc02acfa6023769783b32113b6b76
HumaCode <HumaCode>
2023-09-07 21:22:52 +0700

    membuat method likeOrUnlike - LikeController

--- app/Http/Controllers/Blog/LikeController.php
+++ app/Http/Controllers/Blog/LikeController.php
@@ -3,9 +3,40 @@
 namespace App\Http\Controllers\Blog;
 
 use App\Http\Controllers\Controller;
+use App\Models\Like;
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class LikeController extends Controller
 {
-    //
+    public function likeOrUnlike($id)
+    {
+        $post = Post::find($id);
+
+        // jika tidak ada post
+        if (!$post) {
+            return response([
+                'message' => 'Post not found..',
+            ], 404);
+        }
+
+        $like = $post->likes()->where('user_id', auth()->user()->id)->first();
+
+        if (!$like) {
+            Like::create([
+                'post_id' => $id,
+                'user_id' => auth()->user()->id,
+            ], 200);
+
+            return response([
+                'message' => 'Liked..',
+            ], 200);
+        } else {
+            $like->delete();
+
+            return response([
+                'message' => 'Dislike..',
+            ], 200);
+        }
+    }
 }
