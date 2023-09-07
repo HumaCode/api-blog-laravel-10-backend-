19f9db03e5ecaf49122b9fdf7449ee7cfb648a0f
HumaCode <HumaCode>
2023-09-07 21:09:41 +0700

    membuat method index - CommentController

--- app/Http/Controllers/Blog/CommentController.php
+++ app/Http/Controllers/Blog/CommentController.php
@@ -3,9 +3,24 @@
 namespace App\Http\Controllers\Blog;
 
 use App\Http\Controllers\Controller;
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class CommentController extends Controller
 {
-    //
+    public function index($id)
+    {
+        $post = Post::find($id);
+
+        // jika tidak ada post
+        if (!$post) {
+            return response([
+                'message' => 'Post not found..',
+            ], 404);
+        }
+
+        return response([
+            'post' => $post->comments()->with('user:id,name,email')->get()
+        ]);
+    }
 }
