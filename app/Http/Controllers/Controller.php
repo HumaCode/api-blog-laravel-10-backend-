de90557dad2cb2988282b70d8562a471c2445ca5
HumaCode <HumaCode>
2023-09-09 15:39:07 +0700

    perbaikan upload image

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\Storage;
 use PharIo\Manifest\Url;
+use Illuminate\Support\Str;
 
 class Controller extends BaseController
 {
@@ -13,13 +15,46 @@ class Controller extends BaseController
 
     public function saveImage($image, $path = 'public')
     {
-        if (!$image) {
+        if (!$image || !base64_decode($image, true)) {
             return null;
         }
 
-        $filename = time() . '.png';
-        \Storage::disk($path)->put($filename, base64_decode($image));
+        // Ambil ekstensi gambar dari data gambar yang diterima
+        $imageData = base64_decode($image);
+        $extension = $this->getImageExtension($imageData);
 
-        return Url::to('/') . '/storage/' . $path . '/' . $filename;
+        // Generate filename with the correct extension
+        $filename = time() . '.' . $extension;
+
+        try {
+            // Simpan gambar ke penyimpanan
+            Storage::disk($path)->put($filename, base64_decode($image));
+
+            // Dapatkan URL gambar yang disimpan
+            $url = Storage::url($filename);
+
+            return $url;
+        } catch (\Exception $e) {
+            \Log::error('Gagal menyimpan gambar: ' . $e->getMessage());
+            return null;
+        }
+    }
+
+    // Fungsi untuk mendapatkan ekstensi gambar
+    private function getImageExtension($imageData)
+    {
+        $finfo = new \finfo(FILEINFO_MIME_TYPE);
+        $mime = $finfo->buffer($imageData);
+
+        switch ($mime) {
+            case 'image/png':
+                return 'png';
+            case 'image/jpeg':
+                return 'jpg';
+            case 'image/gif':
+                return 'gif';
+            default:
+                return 'jpg'; // Default extension jika tidak dikenali
+        }
     }
 }
